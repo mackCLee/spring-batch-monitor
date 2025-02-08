21560f31415fb3a23e356005c6de9484b8d8eb69
mackCLee <mack65648@gmail.com>
2025-02-08 12:50:54 +0900

    feat: 실행중인 job 종료 API 추가

--- backend/src/main/java/com/lch275/springBatchMonitor/controller/JobLauncherController.java
+++ backend/src/main/java/com/lch275/springBatchMonitor/controller/JobLauncherController.java
@@ -1,19 +1,24 @@
 package com.lch275.springBatchMonitor.controller;
 
 import com.lch275.springBatchMonitor.service.RedisService;
+import com.lch275.springBatchMonitor.util.BatchUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +26,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import static com.lch275.springBatchMonitor.util.BatchUtil.createJobParameters;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +37,7 @@ public class JobLauncherController {
 
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
+    private final JobExplorer jobExplorer;
     private final JobOperator jobOperator;
     private final RedisService redisService;
 
@@ -41,7 +47,7 @@ public class JobLauncherController {
             Map<String, Object> res = new HashMap<>();
             if(!redisService.acquireLock(jobName)) throw new JobExecutionAlreadyRunningException("acquire lock fail");
             Job job = jobRegistry.getJob(jobName);
-            JobParameters jobParameters = createJobParameters(params);
+            JobParameters jobParameters = BatchUtil.createJobParameters(params);
             JobExecution jobExecution = jobLauncher.run(job, jobParameters);
             redisService.releaseLock(jobName);
             res.put("id", jobExecution.getJobId());
@@ -51,5 +57,28 @@ public class JobLauncherController {
                 JobParametersInvalidException | JobRestartException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @DeleteMapping("/{jobName}")
+    public ResponseEntity<?> stopJob(@PathVariable String jobName, @RequestBody Map<String, Object> params) {
+        Set<JobExecution> jobExecutions = jobExplorer.findRunningJobExecutions(jobName);
+        List<JobExecution> candidates = jobExecutions.stream()
+                .filter(el -> BatchUtil.compareJobParameters(el.getJobParameters(), BatchUtil.createJobParameters(params)))
+                .toList();
+        for(JobExecution jobExecution : candidates) {
+            try {
+                Map<String, Object> res = new HashMap<>();
+                jobOperator.stop(jobExecution.getId());
+                res.put("id", jobExecution.getJobId());
+                res.put("status", jobExecution.getStatus().toString());
+                return ResponseEntity.ok(res);
+            } catch (NoSuchJobExecutionException e) {
+                throw new RuntimeException(e);
+            } catch (JobExecutionNotRunningException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        redisService.releaseLock(jobName);
+        return null;
     }
 }
--- backend/src/main/java/com/lch275/springBatchMonitor/util/BatchUtil.java
+++ backend/src/main/java/com/lch275/springBatchMonitor/util/BatchUtil.java
@@ -1,9 +1,11 @@
 package com.lch275.springBatchMonitor.util;
 
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class BatchUtil {
     public static JobParameters createJobParameters(Map<String, Object> parameters) {
@@ -18,5 +20,40 @@ public class BatchUtil {
             }
         });
         return builder.toJobParameters();
+    }
+
+    public static boolean compareJobParameters(JobParameters p1, JobParameters p2) {
+        if(p1 == null || p2 == null) {
+            return p1 == p2;
+        }
+        Map<String, JobParameter<?>> params1 = p1.getParameters();
+        Map<String, JobParameter<?>> params2 = p2.getParameters();
+
+        // 파라미터 개수 비교
+        if (params1.size() != params2.size()) {
+            return false;
+        }
+        for (Map.Entry<String, JobParameter<?>> entry : params1.entrySet()) {
+            String key = entry.getKey();
+            JobParameter<?> param1 = entry.getValue();
+            JobParameter<?> param2 = params2.get(key);
+
+            // 키가 존재하는지 확인
+            if (param2 == null) {
+                return false;
+            }
+
+            // 타입 비교
+            if (!Objects.equals(param1.getType(), param2.getType())) {
+                return false;
+            }
+
+            // 값 비교
+            if (!Objects.equals(param1.getValue(), param2.getValue())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
